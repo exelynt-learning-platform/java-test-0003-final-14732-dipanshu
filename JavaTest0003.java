84d1fb0b0f1ec88ae3e561aa020b337aa2e1d365
Dipanshu Kubde <77319700+Dipanshu-Kubde@users.noreply.github.com>
2026-03-14 11:37:58 +0000

    Added code for JavaTest0003

---
+++ JavaTest0003.java
@@ -0,0 +1,17 @@
+public class JavaTest0003 {
+
+    public static void main(String[] args) {
+        int rows = 6;
+
+        for (int i = 1; i <= rows; i++) {
+            int value = i % 2;
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(value + " ");
+                value = (value == 1) ? 0 : 1;
+            }
+
+            System.out.println();
+        }
+    }
+}
